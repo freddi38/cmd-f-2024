e78759e1f045d60cd114d01ce1a5ac4fd14162cf
sabrinalou3 <87052249+sabrinalou3@users.noreply.github.com>
2024-03-09 17:28:29 -0800

    changed to mongoose

--- server.js
+++ server.js
@@ -2,39 +2,13 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
+const mongoose = require('Mongoose');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.DATABASE_URL;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-// mongoose.connect(process.env.DATABASE_URL)
-// const db = mongoose.connection
-// db.on('error', (error) => console.error(error))
-// db.once('open', () => console.log('Connected to Database'))
-
-// app.use(express.json())
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
 const runnersRouter = require('./routes/runners')
 app.use('/runners', runnersRouter)
